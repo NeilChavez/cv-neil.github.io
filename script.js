767eed250153282141a506ffd7b6a17212dd2ac9
NeilChavez <98600332+NeilChavez@users.noreply.github.com>
2022-03-30 22:08:14 +0200

    mi comiti

---
+++ script.js
@@ -0,0 +1,63 @@
+/***********  Menu   ***********/  
+
+
+((d)=>{
+    let $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+    $btnMenu.addEventListener( "click", (e) =>{
+
+        $btnMenu.firstElementChild.classList.toggle("none");
+        $btnMenu.lastElementChild.classList.toggle("none");
+        $menu.classList.toggle("is-active");
+
+    });
+
+
+    d.addEventListener("click", e =>{
+        if(!e.target.matches(".menu a")) return false;
+        $btnMenu.firstElementChild.classList.remove("none");
+        $btnMenu.lastElementChild.classList.add("none");
+        $menu.classList.remove("is-active");
+
+    })
+})(document);
+
+
+
+/*************** contact-form ***************/ 
+
+((d)=>{
+    const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/lolochavez22@icloud.com",{ 
+        method: "POST",
+        body: new FormData(e.target)
+    })
+    .then( (res) => (res.ok ?res.json() :Promise.reject(res)))
+    .then( json => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+    }
+        
+    ).catch( err => {
+        console.log(err);
+        let message = err.statusText || "Ocurrio un error, prueba a mandar de nuevo"
+        $response.querySelector(".h3").innerHTML =`Error ${err.status}: ${message}`
+    }).finally(() => {
+        $loader.classList.add("none");
+        setTimeout( ()=>{
+            location.hash = "#close"
+        }, 3000)
+
+    });
+    
+    });
+
+})(document)
